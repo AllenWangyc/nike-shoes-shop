44686c92a062d8e096a46f4b6f96c91c1aca2285
AllenWangyc <451366033@qq.com>
2026-01-24 04:48:05 +1300

    Add responsive layout for navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,37 @@
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react"
 import { RxHamburgerMenu } from "react-icons/rx"
+import { useState } from "react";
 
 const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"]
 export function Nav() {
+  const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
   return <nav className="flex flex-wrap justify-between items-center">
     <a href="#">
       <NikeLogo className="h-20 w-20" />
     </a>
 
-    <button className="p-2 focus:ring-2 focus:ring-gray-200 rounded-lg hover:bg-gray-100">
+    {/* Burger button */}
+    <button className="lg:hidden p-2 focus:ring-2 focus:ring-gray-200 rounded-lg hover:bg-gray-100"
+      onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+    >
       <RxHamburgerMenu size={25} />
     </button>
 
-    <div className="w-full">
-      <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
+    {/* Menu list */}
+    <div className={`${isMobileMenuShown === false && "hidden"} w-full lg:block lg:w-auto`}>
+      <ul className="lg:space-x-8 flex flex-col lg:flex-row bg-gray-50 text-lg border border-gray-100 rounded-lg p-4 lg:bg-transparent lg:border-none">
         {ROUTES.map((route, i) => {
-          return <li className={`rounded cursor-pointer py-2 px-3 ${i === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`} key={route}>{route}</li>
+          return <li className={`rounded cursor-pointer py-2 px-3 ${i === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`} key={route}>{route}</li>
         })}
       </ul>
+    </div>
+
+    {/* Cart button */}
+    <div className="fixed bottom-4 left-4 lg:static">
+      <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+        <TbShoppingBag />
+      </div>
     </div>
   </nav>
 }
