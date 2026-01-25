396c86e596189e25f05078d724c8262572fa0db0
AllenWangyc <451366033@qq.com>
2026-01-26 03:41:12 +1300

    Add Selection component

--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -1,11 +1,13 @@
 import nike1 from "../assets/n1-min.png";
+import { QTY, SIZES } from "../constant";
+import { Select } from "./Select";
 
 export function ShoeDetail() {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
       {/* Shoe image */}
       <div className="flex-1 lg:-mt-32 lg:ml-28">
-        <div className="flex-center h-full bg-linear-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
+        <div className="flex-center  h-full bg-linear-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
           <img src={nike1} alt="" />
         </div>
       </div>
@@ -18,7 +20,12 @@ export function ShoeDetail() {
             "The Nike Air Max 270 is a liftsttyle shoe that's sure to turn heads with ites vibrant color gradient"
           }
         </div>
-        <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+        <div className="flex space-x-6">
+          <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+          <Select title={"QTY"} options={QTY}/>
+          <Select title={"SIZE"} options={SIZES}/>
+        </div>
+        
         {/* Shoe button and links */}
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
