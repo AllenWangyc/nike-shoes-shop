4cffdb68ca284c4e6da2c8745ddec502bf045516
AllenWangyc <451366033@qq.com>
2026-01-27 01:53:37 +1300

    add cart items input

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
 import {IoIosArrowDown} from "react-icons/io"
 
-export function Select({title, options}) {
+export function Select({title=null, options}) {
     return (<div className="relative">
         <select defaultValue={""} className="w-24 appearance-none border border-gray-300 p-4 bg-white">
             <option value="" disabled hidden>{title}</option>
