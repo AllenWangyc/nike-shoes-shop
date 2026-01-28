a0a47b2dc07c55120227ce131bce841bb3888961
AllenWangyc <451366033@qq.com>
2026-01-28 14:01:36 +1300

    Complete dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { SHOE_LIST } from "./constant";
 import { NewArrivalSection } from "./components/NewArrivalSection";
 import { Sidebar } from "./components/SIdebar";
 import { Cart } from "./components/Cart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BiMoon, BiSun} from "react-icons/bi"
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -17,6 +18,22 @@ function App() {
     }
   })
 
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode")
+    if (isDarkMode == "true") {
+      window.document.documentElement.classList.add("dark")
+    }
+  }, [])
+
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark")
+    )
+  }
+
   return (
     <div className="p-10 xl:px-24 animate-fadeIn">
       <Nav onClickShoppingBtn = {() => setIsSidebarOpen(true)} />
@@ -27,6 +44,15 @@ function App() {
       >
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      <div className="fixed bottom-4 right-4">
+        <button 
+          className="bg-night-50 px-4 py-2 rounded-full text-white dark:bg-white dark:text-night cursor-pointer"
+          onClick={toggleDarkMode}
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 }
